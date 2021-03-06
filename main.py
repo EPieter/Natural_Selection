819eb2d31beb9a441ae3653996cd9974db11c20a
Pieter <eikelboom.pieter@gmail.com>
2021-03-06 12:55:18 +0100

    Added functions:     - Added a event to quit when ESC is pressed

--- main.py
+++ main.py
@@ -6,7 +6,23 @@ import time    # import time to build delays and pause running code
 pygame.init()  # initializes PyGame functions
 
 infoObject = pygame.display.Info()  # fetches display size
-pygame.display.set_mode((infoObject.current_w, infoObject.current_h))  # applies display size
+
+# applies display size
+pygame.display.set_mode((infoObject.current_w, infoObject.current_h))
+pygame.display.toggle_fullscreen()
+
+running = True
+while running:  # Run until user asks to quit
+    # Makes the ESC key to close the game
+    for event in pygame.event.get():
+        key_pressed = pygame.key.get_pressed()
+        if key_pressed[pygame.K_ESCAPE]:
+            running = False
+            pygame.quit()
+            sys.exit()
+
+
+
 
 
 
