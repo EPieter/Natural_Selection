755ea89085ecea5463a45330a09c7a93d7f8fa5a
Pieter <eikelboom.pieter@gmail.com>
2021-03-06 11:58:22 +0100

    Added functions:
    Code Syntax Improvement

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-# import pygame for PyGame functionality
-# import sys for properly stopping game and other Python Runtime functionality
-# import time to build delays and pause running code
-import pygame, sys, time
+import pygame  # import pygame for PyGame functionality
+import sys     # import sys for properly stopping game and other Python Runtime functionality
+import time    # import time to build delays and pause running code
 
-pygame.init() # initializes PyGame functions
+
+pygame.init()  # initializes PyGame functions
 
 infoObject = pygame.display.Info()  # fetches display size
 pygame.display.set_mode((infoObject.current_w, infoObject.current_h))  # applies display size
