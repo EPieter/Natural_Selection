83f1be161455f8c0105aab078429463f1d412f2c
Pieter <eikelboom.pieter@gmail.com>
2021-03-06 14:08:31 +0100

    Added Features:     - The quit_event function now runs in a thread in background     - Added logging

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import pygame  # import pygame for PyGame functionality
-import sys     # import sys for properly stopping game and other Python Runtime functionality
-import time    # import time to build delays and pause running code
+import pygame     # import pygame for PyGame functionality
+import sys        # import sys for properly stopping game and other Python Runtime functionality
+import time       # import time to build delays and pause running code
+import threading  # import threading for running multiple functions at once
+import logging    # import logging to log things
 
 
 pygame.init()  # initializes PyGame functions
@@ -8,28 +10,23 @@ pygame.init()  # initializes PyGame functions
 infoObject = pygame.display.Info()  # fetches display size
 
 # applies display size
-pygame.display.set_mode((infoObject.current_w, infoObject.current_h))
+screen = pygame.display.set_mode((infoObject.current_w, infoObject.current_h))
 pygame.display.toggle_fullscreen()
+
 
 running = True
 while running:  # Run until user asks to quit
-    # Makes the ESC key to close the game
-    for event in pygame.event.get():
-        key_pressed = pygame.key.get_pressed()
-        if key_pressed[pygame.K_ESCAPE]:
-            running = False
-            pygame.quit()
-            sys.exit()
+    def quit_event():                       # TODO: Move this function outside of the while loop
+        for event in pygame.event.get():
+            key_pressed = pygame.key.get_pressed()
+            if key_pressed[pygame.K_ESCAPE]:
+                pygame.quit()
+                sys.exit()
 
-
-
-
-
-
-
-
-
-
-
-
-
+    def thread_function(thread_name, success_bool):         # TODO: Move this function outside of the while loop
+        if success_bool:
+            logging.info("Thread %s: successfully started", thread_name)
+        else:
+            logging.info("Thread %s: failed to initialize", thread_name)
+    threading.Thread(target=quit_event(), args=(1,), daemon=True)
+    thread_function("quit_event", 1)
