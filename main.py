e79fd6a0ec8047779c28513eb55a6df6b3ba9b6d
Pieter <eikelboom.pieter@gmail.com>
2021-03-06 11:54:58 +0100

    Added functions:     - PyGame Initialize     - Using screen resolution for game window

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
-import pygame, sys
+# import pygame for PyGame functionality
+# import sys for properly stopping game and other Python Runtime functionality
+# import time to build delays and pause running code
+import pygame, sys, time
+
+pygame.init() # initializes PyGame functions
+
+infoObject = pygame.display.Info()  # fetches display size
+pygame.display.set_mode((infoObject.current_w, infoObject.current_h))  # applies display size
 
 
-print("hello")
-print("het werkt!")
+
+
+
+
+
+
+
+
