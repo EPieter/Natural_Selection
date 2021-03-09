0083d4b0b1f48d7aed92337d7778e3b1558c8919
vandevreede <78849197+vandevreede@users.noreply.github.com>
2021-03-09 08:50:05 +0100

    #0009: started with connection making

--- connection/conn.php
+++ connection/conn.php
@@ -1,6 +1,15 @@
 <?php
+$db_server = "db.nsgame.nl";
+$db_username = "md488088db551267";
+$db_password = "NaturalSelection2021!";
+$db_name = "md488088db551267";
+
+// Create connection
+$conn = mysqli_connect($db_server, $db_username, $db_password, $db_name);
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // collect value of input field
-    echo($_POST['test']);
+    echo($_POST['user']);
+    echo($_POST['password']);
   }
 ?>
--- files/connect_to_cloud.py
+++ files/connect_to_cloud.py
@@ -1,8 +1,24 @@
 from imports import *  # import all modules and files
 
-url = 'http://nsgame.nl/app/conn.php'
-data = {'test': 'connected'}
 
-x = requests.post(url, data=data)
+def check_user():
+    if os.path.exists('../userdata.txt'):
+        f = open('../userdata.txt')
+        file_data = f.read()
+        file_data = file_data.split("|")
+        user = file_data[0]
+        password = file_data[1]
+        url = 'http://nsgame.nl/app/conn.php'
+        url_data_before_sending = {'user': user, 'password': password}
+        x = requests.post(url, data=url_data_before_sending)
 
-print(x.text)
+    else:
+        x = True
+        # TODO: else in this function
+
+    url = 'http://nsgame.nl/app/conn.php'
+    url_data_before_sending = {'user': 'connected'}
+
+    x = requests.post(url, data=url_data_before_sending)
+
+    return x.text
