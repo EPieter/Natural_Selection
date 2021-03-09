f7e7e954f5a1293b246b2c9a7da5c930d7172bb0
vandevreede <78849197+vandevreede@users.noreply.github.com>
2021-03-09 11:35:17 +0100

    Constant data changed from location

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import pygame  # import pygame for PyGame functionality
-import sys  # import sys for properly stopping game and other Python Runtime functionality
-import time  # import time to build delays and pause running code
-import threading  # import threading for running multiple functions at once
-import logging  # import logging to log things
-import common_data as data  # import constant data like colors and sizes
-import classes as cl  # import functions en classes (not yet) from classes.py
+from files.imports import *  # import all modules
 
 # initializes PyGame functions
 pygame.init()
