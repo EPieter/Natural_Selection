162e45bbf3168fe0d3de5234e1c59b4ec5391931
vandevreede <78849197+vandevreede@users.noreply.github.com>
2021-03-09 22:51:26 +0100

    #0023: add new file db.php with connection data

---
+++ connection/db.php
@@ -0,0 +1,9 @@
+<?php
+  $db_server = "db.nsgame.nl";
+  $db_username = "md488088db551267";
+  $db_password = "NaturalSelection2021!";
+  $db_name = "md488088db551267";
+
+  // Create connection
+  $conn = mysqli_connect($db_server, $db_username, $db_password, $db_name);
+?>
