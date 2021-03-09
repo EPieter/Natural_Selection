95e23db418cf6220d809619f3c880c89b9dee643
vandevreede <78849197+vandevreede@users.noreply.github.com>
2021-03-09 22:49:21 +0100

    Some changes:
    #0022: make conn.php safer -> no connection data visible

--- connection/conn.php
+++ connection/conn.php
@@ -1,11 +1,5 @@
 <?php
-$db_server = "db.nsgame.nl";
-$db_username = "md488088db551267";
-$db_password = "NaturalSelection2021!";
-$db_name = "md488088db551267";
-
-// Create connection
-$conn = mysqli_connect($db_server, $db_username, $db_password, $db_name);
+include 'db.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // collect value of input field
