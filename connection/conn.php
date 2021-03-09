d3c7dc885d7fff40da361ab6ff1f0c77e82398dd
vandevreede <78849197+vandevreede@users.noreply.github.com>
2021-03-09 21:52:28 +0100

    New changes: #0021: new case in conn.php "make_new_user"

--- connection/conn.php
+++ connection/conn.php
@@ -12,7 +12,23 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (isset($_POST['method'])) {
         switch ($_POST['method']) {
             case 'get_user_data':
-                echo('get user data');
+                $sql = "
+                    SELECT * FROM ns_users
+                    WHERE user_name = '".$_POST['user']."'
+                ";
+                $sql_n = mysqli_query($conn,$sql);
+                $result_sql = mysqli_fetch_assoc($sql_n);
+                if (mysqli_num_rows($sql_n) == 1) {
+                    if ($_POST['password']==$result_sql['password']) {
+                        echo('access');
+                    } else {
+                        echo('wrong_password');
+                    }
+                } elseif (mysqli_num_rows($sql_n) == 0) {
+                    echo("no_user");
+                } else {
+                    echo("something_went_wrong|1");
+                }
                 break;
             case 'check_if_user_exist':
                 $sql = "
@@ -30,14 +46,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 } elseif (mysqli_num_rows($sql_n) == 0) {
                     echo("no_user");
                 } else {
-                    echo("someting_went_wrong");
+                    echo("something_went_wrong|3");
                 }
                 break;
+            case 'make_new_user':
+                echo('new user');
+                break;
             default:
-                echo('error');
+                echo('something_went_wrong|4');
         }
     } else {
-        echo('There is an error');
+        echo('something_went_wrong|2');
     }
   }
 ?>
