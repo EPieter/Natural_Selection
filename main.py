50c676a460897a9c119c16ea1c82a49671a9a6c2
vandevreede <78849197+vandevreede@users.noreply.github.com>
2021-03-09 11:37:22 +0100

    #0010: Main file changed

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ pygame.display.toggle_fullscreen()
 running = True
 while running:  # Run until running state changes
     # Creates a thread that runs quit_event()
-    cl.threading.Thread(target=cl.quit_event(), args=(1,), daemon=True)
+    func.threading.Thread(target=cl.quit_event(), args=(1,), daemon=True)
     # Send log that thread creation is successful
-    cl.thread_function("quit_event", 1)
+    func.thread_function("quit_event", 1)
 
