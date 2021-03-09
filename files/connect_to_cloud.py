85e32c0fc678d91139210ada96aaeb13c16d5f80
vandevreede <78849197+vandevreede@users.noreply.github.com>
2021-03-09 12:04:11 +0100

    #0012

--- files/connect_to_cloud.py
+++ files/connect_to_cloud.py
@@ -1,19 +1,23 @@
-from imports import *  # import all modules and files
+import os  # for handling files and directories
+import requests  # for post request to the server
 
 
-def check_user():
+def check_user(username, password):
     if os.path.exists('../userdata.txt'):
         f = open('../userdata.txt')
         file_data = f.read()
         file_data = file_data.split("|")
         user = file_data[0]
-        password = file_data[1]
+        passw = file_data[1]
         url = 'http://nsgame.nl/app/conn.php'
-        url_data_before_sending = {'user': user, 'password': password}
+        url_data_before_sending = {'get_user_data': 'True', 'user': user, 'password': passw}
         x = requests.post(url, data=url_data_before_sending)
 
     else:
-        x = True
+        f = open('../userdata.txt', 'w')
+        f.write(username+"|"+password)
+        url = 'http://nsgame.nl/app/conn.php'
+        url_data_before_sending = {'user': username}
         # TODO: else in this function
 
     url = 'http://nsgame.nl/app/conn.php'
