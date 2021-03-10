174df58186b9a44d68d381d4af2734e39e747868
vandevreede <78849197+vandevreede@users.noreply.github.com>
2021-03-10 12:58:44 +0100

    #0031: * started with events controller

--- files/controllers/event_handler.py
+++ files/controllers/event_handler.py
@@ -8,3 +8,12 @@ def quit_event():
         if key_pressed[pg.K_ESCAPE]:
             pg.quit()
             sys.exit()
+
+def mouse_events(event):
+    if event.type == 4:
+        x = True
+
+def events():
+    for event in pg.event.get():
+        if event.type == pg.MOUSEBUTTONDOWN:
+            mouse_events(event)
