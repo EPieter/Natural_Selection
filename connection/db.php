7785413cdf77f44cc6fd97baafefec7fbe6b25f4
vandevreede <78849197+vandevreede@users.noreply.github.com>
2021-05-12 11:55:11 +0200

    #40: No db data visible

--- connection/db.php
+++ connection/db.php
@@ -1,9 +1,1 @@
-<?php
-  $db_server = "db.nsgame.nl";
-  $db_username = "md488088db551267";
-  $db_password = "NaturalSelection2021!";
-  $db_name = "md488088db551267";
 
-  // Create connection
-  $conn = mysqli_connect($db_server, $db_username, $db_password, $db_name);
-?>
