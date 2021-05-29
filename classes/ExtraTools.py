2b63f30d342901894c9e07e032fe7846000601cf
vandevreede <78849197+vandevreede@users.noreply.github.com>
2021-05-29 23:27:53 +0200

    #62: re-add tools

---
+++ classes/ExtraTools.py
@@ -0,0 +1,16 @@
+from PIL import Image
+import os
+
+list_of_files = []
+
+
+def resizeImage(size):
+    for root, dirs, files in os.walk("Sprites/normal/"):
+        for file in files:
+            list_of_files.append(os.path.join(root, file))
+
+    for image in list_of_files:
+        str_img = str(image).split("/")[0] + "/" + str(size) + "px/" + str(image).split("/")[2]
+        img = Image.open(image)
+        img = img.resize((size, size), Image.ANTIALIAS)
+        img.save(str_img)
