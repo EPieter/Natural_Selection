e865945aa6340c3a9f34188c67f51bb6b2aa34f6
vandevreede <78849197+vandevreede@users.noreply.github.com>
2021-06-25 22:53:55 +0200

    Shortcuts

--- classes/Shortcuts.py
+++ classes/Shortcuts.py
@@ -9,9 +9,13 @@ availableShortcuts = [
     ["W or ArrowUp", "Move up"],
     ["S or ArrowDown", "Move down"],
     ["Space", "Select and open store"],
-    ["Q", "Quit store"],
+    ["Q", "Close store"],
+    ["Left Ctrl", "Show available shortcuts"],
+    ["W or ArrowUp", "Select above"],
+    ["D or ArrowDown", "Select below"],
+    ["Return", "Purchase and place building"],
+    ["Delete", "Delete Building"],
 ]
-currentShortcuts = []
 
 
 class Shortcuts(pg.sprite.Sprite):
@@ -19,13 +23,24 @@ class Shortcuts(pg.sprite.Sprite):
         self.groups = game.all_sprites
         pg.sprite.Sprite.__init__(self, self.groups)
         self.game = game
-        self.image = pg.Surface((400, 400))
+        self.image = pg.Surface((600, 400))
         self.image.fill(data.GREEN)
         self.rect = self.image.get_rect()
-        self.rect.x = data.MIDDLE_OF_THE_SCREEN[0] - 200
+        self.rect.x = data.MIDDLE_OF_THE_SCREEN[0] - 300
         self.rect.y = data.MIDDLE_OF_THE_SCREEN[1] - 200
+        self.shortCuts = []
+        self.shortCuts.append(["Q", "Close popup"])
+        for i in self.game.currentShortcuts:
+            self.shortCuts.append(availableShortcuts[i - 1])
 
-        self.space_between_options = 8
+        font = pg.font.Font('resources/OpenSans-SemiBold.ttf', 24)
+        for i in range(len(self.shortCuts)):
+            text = font.render((self.shortCuts[i][0]), True, (255, 255, 255))
+            text_rect = (10 + 0 * 76, 10 + i * 32, 500, 24)
+            self.image.blit(text, text_rect)
+            text = font.render((self.shortCuts[i][1]), True, (255, 255, 255))
+            text_rect = (10 + 3 * 76, 10 + i * 32, 500, 24)
+            self.image.blit(text, text_rect)
 
     def die(self):
         self.kill()
